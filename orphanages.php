6f1f067974c8f939f91e04ad8f236e002ba43854
VijaykumarSJ <22suit17@tcarts.in>
2024-09-09 06:54:44 +0530

    Add files via upload

---
+++ orphanages.php
@@ -0,0 +1,70 @@
+    <?php include 'header.php'; ?>
+   <!-- Page Content-->
+            <div class="container px-5 my-5">
+                <div class="text-center mb-5">
+                    <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Our Orphanages</span></h1>
+                </div>
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-lg-11 col-xl-9 col-xxl-8">
+                        <!-- Experience Section-->
+                        <section>
+                            
+                            <!-- Experience Card 1-->
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <img src="assets/images/orphanage_vector.png" class="w-100">
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8">
+                                                <div class="small fw-bolder">Agaram Foundations</div>
+                                            <div class="text-primary fw-bolder mb-2 mt-2">Physically Challenged</div>
+                                                <div class="small text-muted mb-2">Madurai</div>
+                                                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus laudantium, voluptatem quis repellendus eaque sit animi illo ipsam amet officiis corporis sed aliquam non voluptate corrupti excepturi maxime porro fuga.</div></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <img src="assets/images/orphanage_vector.png" class="w-100">
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8">
+                                                <div class="small fw-bolder">Agaram Foundations</div>
+                                            <div class="text-primary fw-bolder mb-2 mt-2">Physically Challenged</div>
+                                                <div class="small text-muted mb-2">Madurai</div>
+                                                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus laudantium, voluptatem quis repellendus eaque sit animi illo ipsam amet officiis corporis sed aliquam non voluptate corrupti excepturi maxime porro fuga.</div></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <img src="assets/images/orphanage_vector.png" class="w-100">
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8">
+                                                <div class="small fw-bolder">Agaram Foundations</div>
+                                            <div class="text-primary fw-bolder mb-2 mt-2">Physically Challenged</div>
+                                                <div class="small text-muted mb-2">Madurai</div>
+                                                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus laudantium, voluptatem quis repellendus eaque sit animi illo ipsam amet officiis corporis sed aliquam non voluptate corrupti excepturi maxime porro fuga.</div></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </section>
+
+                        <!-- Divider-->
+                        <div class="pb-5"></div>
+                    </div>
+                </div>
+            </div>
